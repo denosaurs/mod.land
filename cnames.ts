1121836c9a29c324075b39e240b71e278f95ce59
Thebigbot <77632836+aThebigbot@users.noreply.github.com>
2022-07-03 10:18:10 +0200

    Update cnames.ts

--- cnames.ts
+++ cnames.ts
@@ -57,9 +57,6 @@ export default <CNAMEs> {
   "cr": {
     target: "rahalkar.dev",
   },
-   "community-changelog": {
-    target: "thebigbot.github.io/community-changelog/",
-  },
   "crewdevio": {
     target: "crewdevio.vercel.app",
   },
